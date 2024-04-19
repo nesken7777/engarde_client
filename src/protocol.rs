d71ed22b0df88cbb6589bfd20a45a0d5c042cac8
nesken7777 <51521915+nesken7777@users.noreply.github.com>
2024-04-19 13:20:02 +0900

    公開するものはする
    公開するものはする

--- src/protocol.rs
+++ src/protocol.rs
@@ -68,13 +68,13 @@ pub struct ConnectionStart {
 #[derive(Serialize)]
 pub struct PlayerName {
     #[serde(rename = "Type")]
-    typ: String,
+    pub typ: String,
     #[serde(rename = "From")]
-    from: String,
+    pub from: String,
     #[serde(rename = "To")]
-    to: String,
+    pub to: String,
     #[serde(rename = "Name")]
-    name: String,
+    pub name: String,
 }
 
 impl PlayerName {
@@ -110,32 +110,32 @@ pub struct BoardInfo {
         rename = "PlayerPosition_0",
         deserialize_with = "deserialize_number_from_string"
     )]
-    player_position_0: u8,
+    pub player_position_0: u8,
     #[serde(
         rename = "PlayerPosition_1",
         deserialize_with = "deserialize_number_from_string"
     )]
-    player_position_1: u8,
+    pub player_position_1: u8,
     #[serde(
         rename = "PlayerScore_0",
         deserialize_with = "deserialize_number_from_string"
     )]
-    player_score_0: u8,
+    pub player_score_0: u8,
     #[serde(
         rename = "PlayerScore_1",
         deserialize_with = "deserialize_number_from_string"
     )]
-    player_score_1: u8,
+    pub player_score_1: u8,
     #[serde(
         rename = "NumofDeck",
         deserialize_with = "deserialize_number_from_string"
     )]
-    num_of_deck: u8,
+    pub num_of_deck: u8,
     #[serde(
         rename = "CurrentPlayer",
         deserialize_with = "deserialize_number_from_string"
     )]
-    current_player: u8,
+    pub current_player: u8,
 }
 
 impl BoardInfo {
@@ -163,15 +163,15 @@ pub struct HandInfo {
     #[serde(rename = "To")]
     to: String,
     #[serde(rename = "Hand1", deserialize_with = "deserialize_number_from_string")]
-    hand1: u8,
+    pub hand1: u8,
     #[serde(rename = "Hand2", deserialize_with = "deserialize_number_from_string")]
-    hand2: u8,
+    pub hand2: u8,
     #[serde(rename = "Hand3", deserialize_with = "deserialize_number_from_string")]
-    hand3: u8,
+    pub hand3: u8,
     #[serde(rename = "Hand4", deserialize_with = "deserialize_number_from_string")]
-    hand4: u8,
+    pub hand4: u8,
     #[serde(rename = "Hand5", deserialize_with = "deserialize_number_from_string")]
-    hand5: u8,
+    pub hand5: u8,
 }
 
 impl HandInfo {
@@ -238,79 +238,79 @@ impl RequestedPlay {
 #[derive(Serialize)]
 pub struct Evaluation {
     #[serde(rename = "Type")]
-    typ: String,
+    pub typ: String,
     #[serde(rename = "From")]
-    from: String,
+    pub from: String,
     #[serde(rename = "To")]
-    to: String,
+    pub to: String,
     #[serde(rename = "1F")]
-    eval_1f: Option<String>,
+    pub eval_1f: Option<String>,
     #[serde(rename = "1B")]
-    eval_1b: Option<String>,
+    pub eval_1b: Option<String>,
     #[serde(rename = "2F")]
-    eval_2f: Option<String>,
+    pub eval_2f: Option<String>,
     #[serde(rename = "2B")]
-    eval_2b: Option<String>,
+    pub eval_2b: Option<String>,
     #[serde(rename = "3F")]
-    eval_3f: Option<String>,
+    pub eval_3f: Option<String>,
     #[serde(rename = "3B")]
-    eval_3b: Option<String>,
+    pub eval_3b: Option<String>,
     #[serde(rename = "4F")]
-    eval_4f: Option<String>,
+    pub eval_4f: Option<String>,
     #[serde(rename = "4B")]
-    eval_4b: Option<String>,
+    pub eval_4b: Option<String>,
     #[serde(rename = "5F")]
-    eval_5f: Option<String>,
+    pub eval_5f: Option<String>,
     #[serde(rename = "5B")]
-    eval_5b: Option<String>,
+    pub eval_5b: Option<String>,
 }
 
 #[derive(Serialize)]
 pub struct PlayMovement {
     #[serde(rename = "Type")]
-    typ: String,
+    pub typ: String,
     #[serde(rename = "From")]
-    from: String,
+    pub from: String,
     #[serde(rename = "To")]
-    to: String,
+    pub to: String,
     #[serde(rename = "MessageID")]
-    message_id: String,
+    pub message_id: String,
     #[serde(rename = "PlayCard")]
-    play_card: String,
+    pub play_card: String,
     #[serde(rename = "Direction")]
-    direction: String,
+    pub direction: String,
 }
 
 #[derive(Serialize)]
 pub struct PlayAttack {
     #[serde(rename = "Type")]
-    typ: String,
+    pub typ: String,
     #[serde(rename = "From")]
-    from: String,
+    pub from: String,
     #[serde(rename = "To")]
-    to: String,
+    pub to: String,
     #[serde(rename = "MessageID")]
-    message_id: String,
+    pub message_id: String,
     #[serde(rename = "PlayCard")]
-    play_card: String,
+    pub play_card: String,
     #[serde(rename = "NumOfCard")]
-    num_of_card: String,
+    pub num_of_card: String,
 }
 
 #[derive(Serialize)]
 pub struct PlayParry {
     #[serde(rename = "Type")]
-    typ: String,
+    pub typ: String,
     #[serde(rename = "From")]
-    from: String,
+    pub from: String,
     #[serde(rename = "To")]
-    to: String,
+    pub to: String,
     #[serde(rename = "MessageID")]
-    message_id: String,
+    pub message_id: String,
     #[serde(rename = "PlayCard")]
-    play_card: String,
+    pub play_card: String,
     #[serde(rename = "NumOfCard")]
-    num_of_card: String,
+    pub num_of_card: String,
 }
 
 #[derive(Deserialize)]
@@ -322,13 +322,13 @@ pub struct RoundEnd {
     #[serde(rename = "To")]
     to: String,
     #[serde(rename = "Winner", deserialize_with = "deserialize_number_from_string")]
-    winner: u8,
+    pub winner: u8,
     #[serde(rename = "Score0", deserialize_with = "deserialize_number_from_string")]
-    score_0: u8,
+    pub score_0: u8,
     #[serde(rename = "Score1", deserialize_with = "deserialize_number_from_string")]
-    score_1: u8,
+    pub score_1: u8,
     #[serde(rename = "Message")]
-    message: String,
+    pub message: String,
 }
 
 #[derive(Deserialize)]
@@ -340,11 +340,11 @@ pub struct GameEnd {
     #[serde(rename = "To")]
     to: String,
     #[serde(rename = "Winner", deserialize_with = "deserialize_number_from_string")]
-    winner: u8,
+    pub winner: u8,
     #[serde(rename = "Score0", deserialize_with = "deserialize_number_from_string")]
-    score_0: u8,
+    pub score_0: u8,
     #[serde(rename = "Score1", deserialize_with = "deserialize_number_from_string")]
-    score_1: u8,
+    pub score_1: u8,
     #[serde(rename = "Message")]
-    message: String,
+    pub message: String,
 }
