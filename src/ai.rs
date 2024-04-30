22801709a58f201e41256f10c25e13394707a07c
nesken7777 <51521915+nesken7777@users.noreply.github.com>
2024-04-30 14:10:40 +0900

    ちょっとだけきれいに

--- src/ai.rs
+++ src/ai.rs
@@ -266,7 +266,7 @@ impl Agent<MyState> for MyAgent {
 }
 
 pub fn ai_main() -> Result<(), Errors> {
-    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 12052);
+    let addr = SocketAddr::from(([127, 0, 0, 1], 12052));
     print("connect?")?;
     read_keyboard()?;
     let stream = TcpStream::connect(addr)?;
